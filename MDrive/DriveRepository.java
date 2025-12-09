c744be160293f637522cff9cdb2f3816ae65d9c6
Diya27barla <144506830+Diya27barla@users.noreply.github.com>
2025-12-09 16:42:12 +0530

    Revert "Addition of Drive, DriveType "

--- MDrive/DriveRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.MDrive;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriveRepository extends JpaRepository<Drive, Integer> {
-}
--- MDriveType/DriveTypeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.MDriveType;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriveTypeRepository extends JpaRepository<DriveType, Integer> {
-}
