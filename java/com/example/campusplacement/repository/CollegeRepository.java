126ff2f3324ea06b55ddecef6efab6de4a966149
Shivani S Nair <shivanisn825@gmail.com>
2025-12-14 19:03:37 +0530

    deleting old structure

--- java/com/example/campusplacement/repository/CollegeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.campusplacement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.campusplacement.entity.College;
-
-public interface CollegeRepository extends JpaRepository<College, Long> {
-}
